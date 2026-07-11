0c95834bdf920a6cda84376680afbd8de7f22d04
usernotfree <lanzallen28@gmail.com>
2026-07-12 02:00:59 +0800

    Replace brand text with logo image in sidebar

--- exchange.php
+++ exchange.php
@@ -23,7 +23,7 @@ $walletPageData = perahp_wallet_page_data($user);
 </head>
 <body>
     <aside class="sidebar" id="sidebar">
-        <a class="brand" href="main.php"><span class="brand-mark">PHP</span><div><strong>PeraHP</strong><small>Digital wallet</small></div></a>
+        <a class="brand" href="main.php"><img class="brand-mark" src="logo.png" width="46" height="46" alt="PeraHP logo"><div><strong>PeraHP</strong><small>Digital wallet</small></div></a>
         <nav class="nav-list">
             <a class="nav-link" href="main.php">Dashboard</a>
             <a class="nav-link" href="wallets.php">Wallets</a>
